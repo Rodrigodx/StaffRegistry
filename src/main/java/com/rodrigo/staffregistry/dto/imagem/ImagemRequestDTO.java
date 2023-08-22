92cd1a7886c53478671a452c0c047c4fff9bdcd5
Rodrigodx <rodrigodx52@gmail.com>
2023-08-22 19:05:14 -0300

    Organizado os ImagemDTOs e adicionado a anotação '@Data'

--- src/main/java/com/rodrigo/staffregistry/dto/imagem/ImagemRequestDTO.java
+++ src/main/java/com/rodrigo/staffregistry/dto/imagem/ImagemRequestDTO.java
@@ -1,7 +1,9 @@
-package com.rodrigo.staffregistry.dto;
+package com.rodrigo.staffregistry.dto.imagem;
 
 import jakarta.persistence.Lob;
+import lombok.Data;
 
+@Data
 public class ImagemRequestDTO {
     private byte[] imagem;
 }
--- src/main/java/com/rodrigo/staffregistry/dto/imagem/ImagemResponseDTO.java
+++ src/main/java/com/rodrigo/staffregistry/dto/imagem/ImagemResponseDTO.java
@@ -1,11 +1,13 @@
-package com.rodrigo.staffregistry.dto;
+package com.rodrigo.staffregistry.dto.imagem;
 
 import jakarta.persistence.Lob;
+import lombok.Data;
 
+@Data
 public class ImagemResponseDTO {
 
     private Integer id;
 
     private byte[] imagem;
-    
+
 }
