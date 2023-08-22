71810700bb19ddfaa806551313dc65fd6b23087b
Rodrigodx <rodrigodx52@gmail.com>
2023-08-22 19:03:17 -0300

    Organizado os ControllersDTOs

--- src/main/java/com/rodrigo/staffregistry/dto/cadastro/CadastroResponseDTO.java
+++ src/main/java/com/rodrigo/staffregistry/dto/cadastro/CadastroResponseDTO.java
@@ -1,4 +1,4 @@
-package com.rodrigo.staffregistry.dto;
+package com.rodrigo.staffregistry.dto.cadastro;
 
 import com.rodrigo.staffregistry.enums.PessoaEnum;
 import com.rodrigo.staffregistry.model.Endereco;
